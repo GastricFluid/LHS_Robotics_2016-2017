60385b60c306d26424cc249ae5b99a46b1205129
GastricFluid <comeaupaul98@gmail.com>
2017-01-21 14:00:50 -0600

    added array analyzer and made some quality of life changes

--- Imaging/angled_corners.py
+++ Imaging/angled_corners.py
@@ -1,9 +1,11 @@
 import numpy
+import numpy.ndarray.item
 import cv2
 import math
 
 
 def AngleCheck(Vector1, Vector2):
+    # finds the angle between two vectors
     x_diff = (Vector1[0] - Vector2[0])
     y_diff = (Vector1[1] - Vector2[1])
     if x_diff != 0:
@@ -11,13 +13,13 @@ def AngleCheck(Vector1, Vector2):
     else:
         comp_rad = math.pi/2
     comp_angle = math.degrees(comp_rad)
-    print y_diff
-    print x_diff
-    print comp_rad
     return comp_angle
 
 
-def ArraySetter():
-    pass
-
-print AngleCheck([5, 0], [6, 1])
+def ArrayAnalyzer(Corner_Array):
+    # finds angles of give vector array
+    Corner_Angle1 = (AngleCheck(Corner_Array.item(0), Corner_Array.item(1)))
+    Corner_Angle2 = (AngleCheck(Corner_Array.item(4), Corner_Array.item(5)))
+    Corner_Angle3 = (AngleCheck(Corner_Array.item(2), Corner_Array.item(3)))
+    Corner_Angle4 = (AngleCheck(Corner_Array.item(6), Corner_Array.item(7)))
+    return (Corner_Angle1, Corner_Angle2, Corner_Angle3, Corner_Angle4)
