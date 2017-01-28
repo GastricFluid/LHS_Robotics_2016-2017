2971be83da21f6a763eb66182a3461b58fcfbec2
thedarknebula <paxarmon@yahoo.com>
2017-01-28 14:55:21 -0700

    Update MecanumDrive.java

--- Movement/MecanumDrive.java
+++ Movement/MecanumDrive.java
@@ -3,3 +3,11 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class MecanumDrive extends IterativeRobot{
+RobotDrive robodriver = new RobotDrive(1, 2, 3, 4);
+     //Define joystick being used at USB port 1 on the Driver Station
+     Joystick driveStick = new Joystick(1);
+
+     public void teleopPeriodic() {
+          robodriver.mecanumDrive_Cartesian((drivestick.getX()/2), (drivestick.getY()/2), drivestick.getTwist(),0);
+     }
+}
