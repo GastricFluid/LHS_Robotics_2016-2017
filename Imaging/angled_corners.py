0ed801fcaf6fac44c907b7e5471abe2887dbbcab
GastricFluid <comeaupaul98@gmail.com>
2017-01-28 12:47:22 -0600

    fixed all the stupid bugs, tested, and everything is working as far as I know

--- Imaging/angled_corners.py
+++ Imaging/angled_corners.py
@@ -1,16 +1,12 @@
 import numpy
-import numpy.ndarray.item
 import math
 
 
 def AngleCheck(Vector1, Vector2):
     # finds the angle between two vectors
-    x_diff = (Vector1[0] - Vector2[0])
-    y_diff = (Vector1[1] - Vector2[1])
-    if x_diff != 0:
-        comp_rad = math.atan2(x_diff, y_diff)
-    else:
-        comp_rad = math.pi/2
+    x_diff = (Vector2[0] - Vector1[0])
+    y_diff = (Vector2[1] - Vector1[1])
+    comp_rad = math.atan2(y_diff, x_diff)
     comp_angle = math.degrees(comp_rad)
     return comp_angle
 
@@ -18,23 +14,23 @@ def AngleCheck(Vector1, Vector2):
 def Dist_Check(Vector1, Vector2):
     # returns distance between two vectors
     x_diff = abs(Vector1[0] - Vector2[0])
-    y_diff = abs(Vector1[0] - Vector2[0])
+    y_diff = abs(Vector1[1] - Vector2[1])
     distance = math.sqrt(x_diff**2 + y_diff**2)
     return distance
 
 
 def ArrayAnalyzer(Corner_Array):
     # finds angles of give vector array
-    Corner_Angle1 = (AngleCheck(Corner_Array.item(0), Corner_Array.item(1)))
-    Corner_Angle2 = (AngleCheck(Corner_Array.item(4), Corner_Array.item(5)))
-    Corner_Angle3 = (AngleCheck(Corner_Array.item(2), Corner_Array.item(3)))
-    Corner_Angle4 = (AngleCheck(Corner_Array.item(6), Corner_Array.item(7)))
+    Corner_Angle1 = (AngleCheck(Corner_Array[0], Corner_Array[1]))
+    Corner_Angle2 = (AngleCheck(Corner_Array[4], Corner_Array[5]))
+    Corner_Angle3 = (AngleCheck(Corner_Array[2], Corner_Array[3]))
+    Corner_Angle4 = (AngleCheck(Corner_Array[6], Corner_Array[7]))
     return (Corner_Angle1, Corner_Angle2, Corner_Angle3, Corner_Angle4)
 
 
 def CloseState(Corner_Array, min_length, max_length):
     # given a minimum line length and maximum side length return when tape is close
-    corner_distance = Dist_Check(Corner_Array(1), Corner_Array(2))
+    corner_distance = Dist_Check(Corner_Array[0], Corner_Array[1])
     if corner_distance <= max_length and corner_distance >= min_length:
         return True
     else:
@@ -49,7 +45,7 @@ def TurnState(Analyzed_Angles):
         angle_total += angle
         angle_num += 1
     compiled_angle = angle_total/angle_num
-    if abs(compiled_angle) >= 85 and abs(compiled_angle):
+    if abs(compiled_angle) <= 5 or abs(compiled_angle >= 185):
         return 0
     else:
         if compiled_angle >= 0:
