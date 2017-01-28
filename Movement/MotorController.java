331e88b50b6d213cae04f52029e6e7d7b3896a2c
cybermage9 <cybermage9@users.noreply.github.com>
2017-01-28 13:11:35 -0700

    Add files via upload

---
+++ Movement/MotorController.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team5763.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+public class MotorController{
+	
+	private SpeedController motor = new Talon(0);
+	
+	private Joystick jStick = new Joystick(0);
+	
+	private final double UpdatePeriod = 0.005;
+}
