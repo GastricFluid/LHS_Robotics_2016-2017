fc748569e393e02d13848785648d8fb410728f14
GastricFluid <comeaupaul98@gmail.com>
2017-01-28 14:03:58 -0600

    added serialized output stuff, needs to be tested

--- Imaging/MainFunction.py
+++ Imaging/MainFunction.py
@@ -1,5 +1,6 @@
 import angled_corners
 import line
+import serial
 
 
 def CornerInput():
@@ -8,15 +9,18 @@ def CornerInput():
 
 def AngleOut(CornerArray):
     analyzed_angles = angled_corners.ArrayAnalyzer(CornerArray)
-    print angled_corners.CompiledAnglePure(analyzed_angles)
+    return angled_corners.CompiledAnglePure(analyzed_angles)
 
 
 def Main():
+    ser = serial.Serial('/dev/ttyAMAO', 9600, timeout=0)
     while True:
         if raw_input():
             break
         given_corners = CornerInput()
-        AngleOut(given_corners)
+        angle = AngleOut(given_corners)
+        ser.write(angle)
+        print angle
 
 if __name__ == '__main__':
     Main()
