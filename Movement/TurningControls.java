2dfd827b2f1278e04802d18aa3a7a382c4995d53
cybermage9 <cybermage9@users.noreply.github.com>
2017-01-28 14:08:10 -0700

    Update TurningControls.java

--- Movement/TurningControls.java
+++ Movement/TurningControls.java
@@ -7,6 +7,32 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 
 public class TurningControls extends IterativeRobot {
-  private static final double kVoltsPerDegreePerSecond = 0.0128;
+  //calibration for turning, may need to be fine tuned
+  private static final double VoltsPerDegreePerSecond = 0.0128;
   
+  private static final int FrontLeftMotorPort = 0;
+	private static final int FrontRightMotorPort = 1;
+	private static final int RearLeftMotorPort = 2;
+	private static final int RearRightMotorPort = 3;
+	private static final int GyroPort = 0;
+	private static final int JoystickPort = 0;
+  
+  private RobotDrive myRobot;
+	private AnalogGyro gyro = new AnalogGyro(GyroPort);
+	private Joystick joystick = new Joystick(JoystickPort);
+  
+  @Override
+	public void robotInit() {
+		myRobot = new RobotDrive(FrontLeftMotorPort, FrontRightMotorPort,
+			RearLeftMotorPort, RearRightMotorPort);
+    myRobot.setInvertedMotor(MotorType.FrontLeft, true);
+		myRobot.setInvertedMotor(MotorType.RearLeft, true);
+    
+    gyro.setSensitivity(VoltsPerDegreePerSecond);
+  }
+  
+  @Override
+	public void teleopPeriodic() {
+		myRobot.mecanumDrive_Cartesian(joystick.getX(), joystick.getY(), joystick.getZ(), gyro.getAngle());
+  }
 }
