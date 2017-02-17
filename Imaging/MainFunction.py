2b98a04cb352758c390f8131e0b530a270e7df0c
Lonnie Snyder <LonnieS@users.noreply.github.com>
2017-02-17 07:23:09 -0700

    Update MainFunction.py

--- Imaging/MainFunction.py
+++ Imaging/MainFunction.py
@@ -15,7 +15,8 @@ def AngleOut(CornerArray):
 
 
 def SerTest():
-	ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+	#ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=0)
+	ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=0)
 	angle = 90
 	ser.write(str(angle).encode())
 	print(ser.readline())
@@ -24,7 +25,8 @@ def SerTest():
 def Main():
 	Camera.init()
 	waitloop = 0
-	ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=0)
+	#ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=0)
+	ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=0)
 	print("ook")
 	while True:
 		print("in while")
